53ed8d5ac030a91028b4ada8d0a02a899e837ba7
tam1009 <trongtam.100902@gmail.com>
2024-05-08 08:44:51 +0700

    add device for model

--- detectors/yolov8.py
+++ detectors/yolov8.py
@@ -2,6 +2,7 @@ from ultralytics import YOLO
 
 class YOLOModel:
     def __init__(self):
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.model = YOLO("C:\\Users\\LENOVO\\Sign_language_translation\\detectors\\weights\\best(3).pt")
 
     def predict(self, input):
